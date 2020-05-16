db1182840efd251f9f65238e1526598952fd0f97
neerajp99 <neerajp1999@gmail.com>
2020-05-17 03:06:09 +0530

    Add initial template for a slide

---
+++ src/templates/post.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+
+const Slide = () => {
+  return (
+    <Layout>
+      <div>Hello World 111</div>
+    </Layout>
+  )
+}
+
+export default Blog
