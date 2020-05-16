77d8425f4d2eb0f182f8c4e66c2f263057989343
neerajp99 <neerajp1999@gmail.com>
2020-05-17 03:06:58 +0530

    Update header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 // import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React, { useState, useEffect } from "react"
+import "../styles/main.css"
 
 // Header goes here
 const Header = ({ siteTitle }) => {
@@ -16,9 +17,11 @@ const Header = ({ siteTitle }) => {
 
     let time =
       hours > 12
-        ? "0" + (hours - 12) + " : " + minutes + " : " + seconds + " " + "PM"
+        ? hours < 22
+          ? "0" + (hours - 12) + " : " + minutes + " : " + seconds + " " + "PM"
+          : hours - 12 + " : " + minutes + " : " + seconds + " " + "PM"
         : hours + " : " + minutes + " : " + seconds + " " + "AM"
-    console.log(time)
+    // console.log(time)
     return time
   }
   // Update time each time
@@ -44,14 +47,16 @@ const Header = ({ siteTitle }) => {
 
   return (
     <header>
-      <div className="grid grid-cols-2 divide-gray-400">
+      <div className="grid grid-cols-2 divide-gray-400 my-3">
         <div className="site_title text-xl text-white p-2">{siteTitle}</div>
         <div className="site_date_time text-xl text-white text-right">
-          <nav className="w-1/3 p-2 flex float-right">
-            <div className="w-1/4 h-12 text-left">{time.slice(0,4)}</div>
-            <div className="w-1/4 h-12 text-center">{time.slice(4,9)}</div>
-            <div className="w-1/4 h-12 text-left">{time.slice(9,13)}</div>
-            <div className="w-1/4 h-12 text-left -mx-2">{time.slice(13,15)}</div>
+          <nav className="w-1/2 p-2 flex float-right nav">
+            <div className="w-1/5 h-12 text-left">{time.slice(0, 4)}</div>
+            <div className="w-1/5 h-12 text-left -mx-2">{time.slice(4, 9)}</div>
+            <div className="w-1/5 h-12 text-left">{time.slice(9, 13)}</div>
+            <div className="w-1/5 h-12 text-left -mx-5">
+              {time.slice(13, 15)}
+            </div>
           </nav>
         </div>
       </div>
