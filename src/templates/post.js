1a9d0df23c8e2c176a5d15dc7f298cfacb225bf0
neerajp99 <neerajp1999@gmail.com>
2020-05-19 09:27:40 +0530

    Add graphql query to fetch content of each slide

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,12 +2,32 @@ import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
 
-const Slide = () => {
+// Fetching the content of a single slide and passing as props
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+      }
+      html
+      excerpt
+    }
+  }
+`
+
+const Post = props => {
   return (
     <Layout>
-      <div>Hello World 111</div>
+      <div className="mx-auto my-8 main-screen bg-blue-500">
+        <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: props.data.markdownRemark.html,
+          }}
+        ></div>
+      </div>
     </Layout>
   )
 }
 
-export default Blog
+export default Post
