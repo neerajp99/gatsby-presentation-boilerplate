503f923da477f4799d2fbe73894aea6703b9ed87
neerajp99 <neerajp1999@gmail.com>
2020-05-21 23:46:09 +0530

    Update homepage with navigaton

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,26 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useEffect } from "react"
+import { Link, navigate } from "gatsby"
+
 import "../styles/styles.scss"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import "../styles/main.css"
 import Layout from "../components/layout"
 
-const IndexPage = () => (
-  <Layout>
-    <div className="container mx-auto my-8 main-screen bg-gray-900 slide-main">
-      <h1>The Joy of creating art with code.</h1>
-      <pre>
-        <code>
-          Neeraj Pandey.
-        </code>
-      </pre>
-    </div>
-  </Layout>
-)
+const IndexPage = () => {
+  // useEffect(() => {
+  //   navigate(`/slides/01`, { replace: true })
+  // }, [])
+  return (
+    <Layout>
+      <div className="container mx-auto my-8 main-screen bg-gray-900 slide-main">
+        <h1>The Joy of creating art with code.</h1>
+        <pre>
+          <code>Neeraj Pandey.</code>
+        </pre>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
