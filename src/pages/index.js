6cb2084a710e20faf0e28265123ec04ccaee8862
neerajp99 <neerajp1999@gmail.com>
2020-05-23 23:44:06 +0530

    Add navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react"
 import { Link, navigate } from "gatsby"
-
 import "../styles/styles.scss"
 import Header from "../components/header"
 import Footer from "../components/footer"
@@ -8,18 +7,19 @@ import "../styles/main.css"
 import Layout from "../components/layout"
 
 const IndexPage = () => {
-  // useEffect(() => {
-  //   navigate(`/slides/01`, { replace: true })
-  // }, [])
+ useEffect(() => {
+  navigate(`/slides/1`, { replace: true })
+}, [])
+
   return (
-    <Layout>
-      <div className="container mx-auto my-8 main-screen bg-gray-900 slide-main">
-        <h1>The Joy of creating art with code.</h1>
-        <pre>
-          <code>Neeraj Pandey.</code>
-        </pre>
-      </div>
-    </Layout>
+    <div></div>
+    /*{<div className="container mx-auto my-8 main-screen bg-gray-900 slide-main">
+      <h1>The Joy of creating art with code.</h1>
+      <pre>
+        <code>Neeraj Pandey.</code>
+        <Link to={"/slides"}> Slides </Link>
+      </pre>
+    </div>}*/
   )
 }
 
