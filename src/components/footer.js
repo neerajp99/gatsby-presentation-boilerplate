6156d00b22dd5a055f6ea00b6a109eed4ccfb95e
neerajp99 <neerajp1999@gmail.com>
2020-05-27 01:14:39 +0530

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -19,14 +19,14 @@ const Footer = () => {
 
   return (
     <div className="grid grid-cols-2 divide-gray-400 my-3 bg-gray-900 h-10">
-      <div className="site_title text-white p-2 sm:text-base md:text-l lg:text-sm xl:text-sx ">
+      <div className="footer_left site_title text-white p-2 sm:text-base md:text-l lg:text-sm xl:text-sx ">
         <span className="ml-3 flex -ml-2">
           {tagList.map(content => (
             <div className="flex-grow-0 ml-4">#{content}</div>
           ))}
         </span>
       </div>
-      <div className="site_title text-l text-white p-2">
+      <div className="footer_right site_title text-l text-white p-2">
         <span className="float-right sm:text-base md:text-l lg:text-sm xl:text-sx mx-3">
           {data.site.siteMetadata.twitter}
         </span>
